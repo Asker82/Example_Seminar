cd8b05cb98ac8c2bf894d0f1e780c2a797686d63
Asker82 <kkerra@mail.ru>
2023-04-25 11:51:02 +0300

    Lesson1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* int number1 = 5;
+double number2 = 12.65;
+
+string word = "my word";
+char symbol = 't';
+
+bool check = false;
+int num1 = 5;
+int num2 = 7;
+Console.WriteLine("My numbers are " + num1 + " and " + num2 + " and it is good!");
+Console.WriteLine($"My numbers are {num1} and {num2} and it is good!"); */
+
+// Console.Write("input a number: ");
+// int num = Convert.ToInt32 (Console.ReadLine());
+
+// string word = Console.ReadLine();
+// Console.WriteLine("Your number is " + num);
+
