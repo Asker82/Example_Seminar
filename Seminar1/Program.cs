ac87a0b42f16d19e7b294fec0dc9a45bb295e0b0
Asker82 <kkerra@mail.ru>
2023-04-25 12:38:17 +0300

    Task 3

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -3,18 +3,48 @@
 // Task 1. Напишите программу, которая на вход два числа и проверяет, 
 // является ли первое число квадратом второго.
 
-Console.Write("input + first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input + first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-int quad2 = num2 * num2;
+// int quad2 = num2 * num2;
 
-if(num1 == quad2)
-{
-    Console.WriteLine("Yes!");
-}
-else
-{
-    Console.WriteLine("No!");
-}
+// if(num1 == quad2)
+// {
+//     Console.WriteLine("Yes!");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+// Task 2. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("input a positive number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = -num;
+
+// while(current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+
+// Task 3
+
+// 32145 / 10 = 3214
+// 32145 / 100 = 321
+// 32145 / 1000 = 32
+// 32145 / 10000 = 3
+
+// 32145 % 10 = 5
+// 32145 % 100 = 45
+// 32145 % 1000 = 145
+// 32145 % 10000 = 2145
+
+// 72635 - > 26
+
+// 72635 % 10000 = 2635 / 100 = 26
